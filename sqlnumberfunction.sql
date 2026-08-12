b230a4bcf9d68fee954e98958d384595bb003e54
pankaj-2021 <pankajmunda269@gmail.com>
2026-08-12 13:36:08 +0530

    Add files via upload

---
+++ sqlnumberfunction.sql
@@ -0,0 +1,53 @@
+  use world;
+  
+  select 34.58, round(34.58);
+  
+  select 34.58, round(34.89);
+  
+  select 34.58, round(34.48);
+  
+  select 134.58, round(134.58,1);
+  
+  select 674.589, round(674.589,2);
+  
+  select 674.5292, round(674.5292,1); 
+  
+  select 9, round(9.2,-1);
+  
+  select 56.89, round(56.89,-1);
+  
+  select 54.39, round(54.39,-1);
+  
+  select 56.89, round(56.89,-2);
+  
+  select 26.89, round(26.89,-2);
+  
+  select 345.87, round(345.87,-3);
+  
+  
+  select 845.87, round(845.87,-3);
+  
+  select 6345.87, round(6345.87,-3);
+  
+  select round(345.87,1), truncate(345.87,1);
+  
+ select mod(4,2);
+ 
+ select mod(3,5);
+ 
+ select floor(34.99);
+ 
+ select ceil(56.0008);
+ 
+ select count(5698);
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
